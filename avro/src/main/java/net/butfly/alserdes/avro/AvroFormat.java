94c3bd2a6fe74d097b3a513ead2534732d9f5138
कलविङ्क <zhangx@hzcominfo.com>
2019-01-16 17:54:17 +0800

    fix and optimize avro

--- avro/src/main/java/net/butfly/alserdes/avro/AvroFormat.java
+++ avro/src/main/java/net/butfly/alserdes/avro/AvroFormat.java
@@ -1,69 +1,152 @@
 package net.butfly.alserdes.avro;
 
-import com.twitter.bijection.Injection;
-import com.twitter.bijection.avro.GenericAvroCodecs;
-import net.butfly.albacore.utils.logger.Loggable;
+import static net.butfly.albacore.utils.collection.Colls.empty;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.BINARY;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.BOOL;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.CHAR;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.DATE;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.DOUBLE;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.FLOAT;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.GEO;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.INT;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.JSON_STR;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.LONG;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.STR;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.STRL;
+import static net.butfly.albatis.ddl.vals.ValType.Flags.UNKNOWN;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.file.FileReader;
+import org.apache.avro.file.SeekableByteArrayInput;
+import org.apache.avro.file.SeekableInput;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import net.butfly.albacore.utils.collection.Maps;
+import net.butfly.albacore.utils.logger.Logger;
 import net.butfly.albatis.ddl.FieldDesc;
 import net.butfly.albatis.ddl.TableDesc;
 import net.butfly.albatis.io.Rmap;
 import net.butfly.albatis.io.format.RmapFormat;
 import net.butfly.alserdes.SerDes;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import static net.butfly.albacore.utils.collection.Colls.empty;
-import static net.butfly.albatis.ddl.vals.ValType.Flags.*;
 
 @SerDes.As("avro")
-public class AvroFormat extends RmapFormat implements Loggable {
+public class AvroFormat extends RmapFormat {
 	private static final long serialVersionUID = 3687957634350865452L;
+	private static final Logger logger = Logger.getLogger(AvroFormat.class);
+	private static final Map<String, Schema> SCHEMAS = Maps.of();
+	private static final Map<String, DataFileWriter<GenericRecord>> WRITERS = Maps.of();
+	private static final Map<String, DatumReader<GenericRecord>> READERS = Maps.of();
 
 	@Override
-	public Rmap ser(Rmap src, TableDesc dst) {
-		Rmap rmap = new Rmap();
-		if (null == dst.fields() || dst.fields().length == 0) return ser(src);
-		Schema schema = getSchema(dst.fields());
-		Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
-		GenericData.Record avroRecord = new GenericData.Record(schema);
-		src.map().forEach(avroRecord::put);
-		byte[] avroRecordBytes = recordInjection.apply(avroRecord);
-		if (null != avroRecordBytes) rmap.put(src.table(), avroRecordBytes);
-		logger().info("Avro format ser " + rmap);
-		rmap.table(src.table());
-		return rmap;
+	public Rmap ser(Rmap m, TableDesc dst) {
+		if (empty(dst.fields())) return ser(m);
+		Schema schema = SCHEMAS.computeIfAbsent(dst.name, t -> avroSchema(dst.fields()));
+		DataFileWriter<GenericRecord> w = WRITERS.computeIfAbsent(dst.name, t -> new DataFileWriter<>(new GenericDatumWriter<GenericRecord>(
+				schema)));
+		return avroSer(m, schema, w);
 	}
 
 	@Override
-	public Rmap deser(Rmap map, TableDesc dst) {
-		Rmap rmap = new Rmap();
-		Schema schema;
-		if (null == dst.fields() || dst.fields().length == 0) return deser(map);
-		schema = getSchema(dst.fields());
-		Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
-		map.map().forEach((k, v) -> {
-			GenericRecord genericRecord = recordInjection.invert((byte[]) v).get();
-			if (null != genericRecord) rmap.put(k, genericRecord);
-		});
-		logger().info("Avro format deser " + rmap);
-		rmap.table(map.table());
-		return rmap;
+	public Rmap ser(Rmap src) {
+		if (empty(src)) return null;
+		Schema schema = avroSchema(src);
+		return avroSer(src, schema, new DataFileWriter<>(new GenericDatumWriter<GenericRecord>(schema)));
 	}
 
-	private Schema getSchema(FieldDesc... fields) {
+	@Override
+	public Rmap deser(Rmap m, TableDesc dst) {
+		if (empty(m)) return null;
+		if (empty(dst.fields())) return deser(m);
+		Schema schema = SCHEMAS.computeIfAbsent(dst.name, k -> avroSchema(dst.fields()));
+		DatumReader<GenericRecord> r = READERS.computeIfAbsent(dst.name, t -> new GenericDatumReader<GenericRecord>(schema));
+		return avroDeser(m, r);
+
+	}
+
+	@Override
+	public Rmap deser(Rmap m) {
+		if (empty(m)) return null;
+		throw new UnsupportedOperationException("Deserialization as avro without schema (FieldDesc) is not supported.");
+		// Schema schema = schema(m);
+		// DatumReader<GenericRecord> r = new GenericDatumReader<GenericRecord>(schema);
+		// return deserBySchema(m, r);
+	}
+
+	@Override
+	public Rmap sers(List<Rmap> l, TableDesc dst) {
+		throw new UnsupportedOperationException("Schemaness record list format not implemented now.");
+	}
+
+	@Override
+	public List<Rmap> desers(Rmap rmap, TableDesc dst) {
+		throw new UnsupportedOperationException("Schemaness record list format not implemented now.");
+	}
+
+	@Override
+	public Class<?> formatClass() {
+		return byte[].class;
+	}
+
+	/**
+	 * it WRONG!
+	 */
+	@Deprecated
+	protected static Schema wrongSchema(Map<String, Object> dst) {
 		List<Schema.Field> fieldList = new ArrayList<>();
-		for (FieldDesc f : fields)
-			fieldList.add(getSchemaField(f));
+		dst.forEach((k, v) -> {
+			if (v instanceof byte[]) {
+				ByteArrayInputStream bi = new ByteArrayInputStream((byte[]) v);
+				ObjectInputStream oi = null;
+				try {
+					oi = new ObjectInputStream(bi);
+					Object obj = oi.readObject();
+					Schema.Field schemaField = avroField(k, obj);
+					fieldList.add(schemaField);
+				} catch (Exception e) {
+					logger.error("byteArray transform object error", e);
+				}
+			}
+		});
 		return Schema.createRecord(fieldList);
 	}
 
-	private Schema.Field getSchemaField(FieldDesc f) {
+	private static Schema avroSchema(FieldDesc... fields) {
+		List<Schema.Field> fieldList = new ArrayList<>();
+		for (FieldDesc f : fields)
+			fieldList.add(avroField(f));
+		Schema s = Schema.createRecord(fieldList);
+		logger.info("Schema build from field list: \n\t" + s.toString());
+		return s;
+	}
+
+	private static Schema avroSchema(Map<String, Object> src) {
+		List<Schema.Field> fieldList = new ArrayList<>();
+		src.forEach((k, v) -> {
+			Schema.Field schemaField = avroField(k, v);
+			fieldList.add(schemaField);
+		});
+		Schema s = Schema.createRecord(fieldList);
+		logger.trace("Schema build from field list: \n\t" + s.toString());
+		return s;
+	}
+
+	private static Schema.Field avroField(FieldDesc f) {
 		if (null == f) return null;
 		switch (f.type.flag) {
 		case BINARY:
@@ -90,27 +173,7 @@ public class AvroFormat extends RmapFormat implements Loggable {
 		return null;
 	}
 
-	@Override
-	public Rmap ser(Rmap src) {
-		if (empty(src)) return null;
-		Rmap rmap = new Rmap();
-		List<Schema.Field> fieldList = new ArrayList<>();
-		src.forEach((k, v) -> {
-			Schema.Field schemaField = getSchemaField(k, v);
-			fieldList.add(schemaField);
-		});
-		Schema schema = Schema.createRecord(fieldList);
-		Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
-		GenericData.Record avroRecord = new GenericData.Record(schema);
-		src.forEach(avroRecord::put);
-		byte[] avroRecordBytes = recordInjection.apply(avroRecord);
-		if (null != avroRecordBytes) rmap.put(src.table(), avroRecordBytes);
-		logger().info("Avro format ser " + rmap);
-		rmap.table(src.table());
-		return rmap;
-	}
-
-	private Schema.Field getSchemaField(String name, Object type) {
+	private static Schema.Field avroField(String name, Object type) {
 		if (null == name && null == type) return null;
 		if (type instanceof byte[]) return new Schema.Field(name, Schema.create(Schema.Type.BYTES), null, new byte[0]);
 		else if (type instanceof Integer) return new Schema.Field(name, Schema.create(Schema.Type.INT), null, 0);
@@ -122,48 +185,36 @@ public class AvroFormat extends RmapFormat implements Loggable {
 		else throw new UnsupportedOperationException("This schema type  is unsupported");
 	}
 
-	@Override
-	public Rmap deser(Rmap dst) {
-		if (empty(dst)) return null;
-		Rmap rmap = new Rmap();
-		List<Schema.Field> fieldList = new ArrayList<>();
-		dst.forEach((k, v) -> {
-			if (v instanceof byte[]) {
-				ByteArrayInputStream bi = new ByteArrayInputStream((byte[]) v);
-				ObjectInputStream oi = null;
-				try {
-					oi = new ObjectInputStream(bi);
-					Object obj = oi.readObject();
-					Schema.Field schemaField = getSchemaField(k, obj);
-					fieldList.add(schemaField);
-				} catch (Exception e) {
-					logger().error("byteArray transform object error", e);
-				}
-			}
-		});
-		Schema schema = Schema.createRecord(fieldList);
-		Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
-		dst.map().forEach((k, v) -> {
-			GenericRecord genericRecord = recordInjection.invert((byte[]) v).get();
-			if (null != genericRecord) rmap.put(k, genericRecord);
-		});
-		logger().info("Avro format deser " + rmap);
-		rmap.table(dst.table());
-		return rmap;
+	private Rmap avroSer(Rmap m, Schema schema, DataFileWriter<GenericRecord> writer) {
+		GenericData.Record rec = new GenericData.Record(schema);
+		m.map().forEach(rec::put);
+		byte[] avro;
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+			writer.create(schema, out);
+			writer.append(rec);
+			avro = out.toByteArray();
+		} catch (Exception e) {
+			return null;
+		}
+		if (avro.length == 0) return null;
+		Rmap r = m.skeleton();
+		r.put(UUID.randomUUID().toString(), avro);
+		return r;
 	}
 
-	@Override
-	public Rmap sers(List<Rmap> l, TableDesc dst) {
-		throw new UnsupportedOperationException("Schemaness record list format not implemented now.");
-	}
-
-	@Override
-	public List<Rmap> desers(Rmap rmap, TableDesc dst) {
-		throw new UnsupportedOperationException("Schemaness record list format not implemented now.");
-	}
-
-	@Override
-	public Class<?> formatClass() {
-		return byte[].class;
+	private Rmap avroDeser(Rmap m, DatumReader<GenericRecord> reader) {
+		if (m.size() > 1) logger().warn("Multiple binary values of one record is not supported.");
+		byte[] v = (byte[]) m.entrySet().iterator().next().getValue();
+		try (SeekableInput in = new SeekableByteArrayInput((byte[]) v);
+				FileReader<GenericRecord> fr = DataFileReader.openReader(in, reader);) {
+			if (!fr.hasNext()) return null;
+			GenericRecord rec = fr.next();
+			if (null == rec || null == rec.getSchema() || empty(rec.getSchema().getFields())) return null;
+			Rmap mm = m.skeleton();
+			rec.getSchema().getFields().forEach(field -> mm.put(field.name(), rec.get(field.name())));
+			return mm;
+		} catch (IOException e) {
+			return null;
+		}
 	}
 }
