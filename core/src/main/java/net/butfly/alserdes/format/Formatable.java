089ba54e3278b6499ff7bdb559b4f1e54760ba77
कलविङ्क <zhangx@hzcominfo.com>
2019-05-07 19:03:20 +0800

    fix minor null exception on formats parsing

--- core/src/main/java/net/butfly/alserdes/format/Formatable.java
+++ core/src/main/java/net/butfly/alserdes/format/Formatable.java
@@ -22,8 +22,8 @@ public interface Formatable extends Loggable {
 
 		SerDes.As[] defs = as(getClass());
 		if (defs.length > 1) logger().warn("Multiple default serdes as annotations marked on " + getClass().getName() + ", first ["
-				+ Annotations.toString(defs[0]) + "] will be used. All serdes as: \n\t" + String.join("\n\t", Colls.list(
-						Annotations::toString, defs)));
+				+ Annotations.toString(defs[0]) + "] will be used. All serdes as: \n\t" + String.join("\n\t", Colls.list(Annotations::toString,
+						defs)));
 		Format def = defs.length == 1 ? of(defs[0].value()) : null;
 		if (null != def && ConstFormat.class.isAssignableFrom(def.getClass())) def = null;
 		Format fmt1 = empty(fmts) ? null : fmts.get(0);
@@ -33,7 +33,7 @@ public interface Formatable extends Loggable {
 			return Colls.list(def);
 		}
 		if (defs.length > 0) logger().info("Default format [" + def.as().value() + "] is ignored by [" //
-				+ String.join(",", Colls.list(fmts, f -> f.as().value())) + "]");
+				+ String.join(",", Colls.list(fmts, f -> null == f || null == f.as() ? null : f.as().value())) + "]");
 		return fmts;
 	}
 }
